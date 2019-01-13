7cf62b8c68eb2ed1a539480a98645bc893323308
youness_chaoui <younesschaoui15@gmail.com>
2019-01-13 22:39:02 +0000

    update 9

--- src/main/java/com/ensa/entities/Film_Affiche_PK.java
+++ src/main/java/com/ensa/entities/Film_Affiche_PK.java
@@ -15,12 +15,12 @@ public class Film_Affiche_PK implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@JsonBackReference
-	@ManyToOne(fetch=FetchType.LAZY)
+	@ManyToOne(fetch=FetchType.EAGER)
 	@JoinColumn(name="film")
 	private Film film;
 	
 	@JsonBackReference
-	@ManyToOne(fetch=FetchType.LAZY)
+	@ManyToOne(fetch=FetchType.EAGER)
 	@JoinColumn(name="affiche")
 	private Affiche affiche;
 
